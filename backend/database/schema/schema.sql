1c8c18c00ce6a27b0aecb7c7d19f0babb825f817
Ricardo Perera <ricardoperera@Ricardos-MacBook-Pro.local>
2025-10-28 19:56:09 +0100

    fix/db schema: removing coordinates_id attribute

--- backend/database/schema/schema.sql
+++ backend/database/schema/schema.sql
@@ -21,14 +21,12 @@ CREATE TABLE IF NOT EXISTS raw_layer(
 CREATE TABLE IF NOT EXISTS processed_layer(
     id VARCHAR(500) PRIMARY KEY NOT NULL,
     raw_layer_id VARCHAR(500) NOT NULL,
-    coordinates_id UUID NOT NULL,
     general_statistics JSON NOT NULL,
     weather_statistics JSON NOT NULL,
     precipitation_statistics JSON NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
 
-    CONSTRAINT fk_raw_layer_id FOREIGN KEY (raw_layer_id) REFERENCES raw_layer(id) ON DELETE CASCADE,
-    CONSTRAINT fk_coordinates_id FOREIGN KEY (coordinates_id) REFERENCES coordinates(id) ON DELETE CASCADE
+    CONSTRAINT fk_raw_layer_id FOREIGN KEY (raw_layer_id) REFERENCES raw_layer(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS transactions(

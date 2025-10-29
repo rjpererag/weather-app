1d43b5d7a8a3885856026821213f4c6d51f545c3
Ricardo Perera <ricardoperera@Ricardos-MacBook-Pro.local>
2025-10-29 17:43:34 +0100

    temporarly main file to test endpoints

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+
+def main():
+
+    print("TEST API")
+    test = requests.get('http://localhost:5001/test')
+    print(test.json())
+    print("-"*45)
+
+    print("TEST COORDINATES")
+    coordinates = requests.get('http://localhost:5001/coordinates/madrid')
+    print(coordinates.json())
+    print("-"*45)
+
+    print("TEST MONITOR ID")
+    monitor_id = requests.post(
+        'http://localhost:5001/weather-data/40.4165/-3.70256/2024-01-01/2024-01-02'
+    )
+    monitor_id_json = monitor_id.json()
+    print(monitor_id.json())
+    print("-"*45)
+
+    print("TEST STATUS")
+    status = requests.get(f'http://localhost:5001/get-status/{monitor_id_json["id_to_monitor"]}')
+    print(status.json())
+    print("-"*45)
+
+    print("TEST SEARCH RESULTS")
+    results = requests.get(
+        f'http://localhost:5001/search-results/{monitor_id_json["id_to_monitor"]}'
+    )
+    print(results.json())
+    print("-"*45)
+
+
+    print("TEST CITY-STATS")
+    data = requests.get(
+        'http://localhost:5001/city-stats/lisboa/2024-01-01/2024-01-02'
+    )
+    print(data.json())
+    print("-"*45)
+
+if __name__ == '__main__':
+    main()
